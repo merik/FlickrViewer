939ec98ce9af04b7fc10095f14fa9d644d47f805
Chi Mai <chi.mai@naamtech.com>
2018-04-10 22:43:20 +1000

    Add pinch to zoom feature

--- FlickrViewer/Views/PhotoMainView/FlickrMainViewerCell.swift
+++ FlickrViewer/Views/PhotoMainView/FlickrMainViewerCell.swift
@@ -13,16 +13,27 @@ class FlickrMainViewerCell: UICollectionViewCell {
 
     private var photo: PhotoViewData!
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var photoImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        scrollView.delegate = self
+        scrollView.contentSize = photoImageView.frame.size
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 10.0
     }
     func setPhoto(_ photo: PhotoViewData) {
         self.photo = photo
         photoImageView.sd_setShowActivityIndicatorView(true)
         photoImageView.sd_setIndicatorStyle(.gray)
         photoImageView.sd_setImage(with: URL(string: photo.url), placeholderImage: nil)
+        scrollView.zoomScale = 1.0
     }
 
+}
+extension FlickrMainViewerCell: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
 }
